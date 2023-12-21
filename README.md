fd74214272f75597f158a25f59731ef4a467e36a
serg3295 <75196080+serg3295@users.noreply.github.com>
2023-12-21 16:42:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+A Visual Studio code extension that allows you to easily navigate between the nearest brackets and quotation marks in the source code.
+
+This extension is highly inspired by [jump-brackets](https://github.com/syovchev/jump-brackets).
+
+Main differences to the "jump-brackets" extension:
+
+- Heavily refactored
+- Fixed bugs
+- Removed selection feature
+
+## Usage
+
+- `hop-brackets.forward` Go to next bracket or quotes.
+- `hop-brackets.backwards` Go to previous bracket or quotes.
+
+ℹ️ No default keybindings are provided by this extension - you'll have to bind the commands yourself.
+
+## Requirements
+
+VS Code version >= 1.75.0
---
+++ src/extension.ts
@@ -0,0 +1,81 @@
+import { ExtensionContext, Position, Selection, TextDocument, TextEditor, commands, window } from "vscode"
+
+type FinderFunc = (currentPosition: Position, lineContent: string, document: TextDocument) => Position
+
+export function activate(context: ExtensionContext): void {
+  const targets = ["(", ")", "{", "}", "[", "]", '"', "'"]
+
+  const findBracketForward = (currentPosition: Position, lineContent: string, document: TextDocument): Position => {
+    const { line, character } = currentPosition
+    let lineNumber = currentPosition.line
+    let charNumber = currentPosition.character
+
+    while (lineNumber < document.lineCount) {
+      while (charNumber < lineContent.length) {
+        const currentCharacter = lineContent[charNumber]
+        charNumber += 1
+
+        if (targets.includes(currentCharacter)) {
+          return new Position(lineNumber, charNumber)
+        }
+      }
+
+      if (lineNumber === document.lineCount - 1) {
+        return new Position(line, character)
+      }
+
+      lineNumber += 1
+      lineContent = lineNumber <= document.lineCount ? document.lineAt(lineNumber).text : ""
+      charNumber = 0
+    }
+    return new Position(line, character)
+  }
+
+  const findBracketBackwards = (currentPosition: Position, lineContent: string, document: TextDocument): Position => {
+    const { line, character } = currentPosition
+    let lineNumber = currentPosition.line
+    let charNumber = currentPosition.character
+
+    while (lineNumber > -1) {
+      while (--charNumber >= 0) {
+        const currentCharacter = lineContent[charNumber]
+
+        if (targets.includes(currentCharacter)) {
+          return new Position(lineNumber, charNumber)
+        }
+      }
+
+      lineNumber -= 1
+
+      if (lineNumber === -1) {
+        return new Position(line, character)
+      }
+
+      lineContent = document.lineAt(lineNumber).text
+      charNumber = lineContent.length
+    }
+    return new Position(line, character)
+  }
+
+  const hop = (findBracket: FinderFunc): void => {
+    const editor: TextEditor | undefined = window.activeTextEditor
+
+    if (!editor) {
+      return
+    }
+
+    const { document } = editor
+    const currentPosition = editor.selection.active
+    const lineContent = document.lineAt(currentPosition.line).text ?? ""
+    const bracketPosition = findBracket(currentPosition, lineContent, document)
+
+    editor.selection = new Selection(bracketPosition, bracketPosition)
+    editor.revealRange(document.lineAt(bracketPosition).range)
+  }
+
+  context.subscriptions.push(commands.registerCommand("hop-brackets.forward", () => hop(findBracketForward)))
+  context.subscriptions.push(commands.registerCommand("hop-brackets.backwards", () => hop(findBracketBackwards)))
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export function deactivate(): void {}
